a2707410b19d66b032ff9aa614e8358b3606ea11
Sharingan616 <sharingan616@gmail.com>
2013-05-11 22:34:22 -0700

    client proxy class

---
+++ client/ClientProxySharConsole.java
@@ -0,0 +1,11 @@
+package sharConsole.client;
+
+import sharConsole.CommonProxySharConsole;
+
+public class ClientProxySharConsole  extends CommonProxySharConsole{
+	@Override
+	public void registerRenderers() {
+		// TODO Auto-generated method stub
+		
+	}
+}
